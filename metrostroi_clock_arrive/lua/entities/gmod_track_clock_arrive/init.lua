f0d9146781148a08f6b9841184e5f07e9a5e9870
Agent Smith <89154459+angelus1637@users.noreply.github.com>
2021-08-19 23:01:50 +0300

    Update init.lua

--- metrostroi_clock_arrive/lua/entities/gmod_track_clock_arrive/init.lua
+++ metrostroi_clock_arrive/lua/entities/gmod_track_clock_arrive/init.lua
@@ -47,7 +47,7 @@ function ENT:Think()
 		
 		local TrainPath = train:ReadCell(49170)
 		local TrainStation = train:ReadCell(49160)
-		if not TrainStation then TrainStation = train:ReadCell(49161)
+		if not TrainStation then TrainStation = train:ReadCell(49161) end
 		local StationPath = self.Path
 		if StationPath != TrainPath then continue end
 		if math.abs(self.Station - TrainStation) > 3 then continue end

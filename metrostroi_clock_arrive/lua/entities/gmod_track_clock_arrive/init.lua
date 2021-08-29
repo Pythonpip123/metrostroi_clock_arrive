0bd79c273d7f3b4f699c7a7e99e14eb097e96283
Agent Smith <89154459+angelus1637@users.noreply.github.com>
2021-08-29 07:47:40 +0300

    Update init.lua

--- metrostroi_clock_arrive/lua/entities/gmod_track_clock_arrive/init.lua
+++ metrostroi_clock_arrive/lua/entities/gmod_track_clock_arrive/init.lua
@@ -9,6 +9,51 @@ local function FindPlatform(st,path)
 	end
 end
 
+local function GetTravelTime(src,dest)
+	-- Determine direction of travel
+	local direction = src.x < dest.x
+
+	-- Accumulate travel time
+	local travel_time = 0
+	local travel_dist = 0
+	local avg_speed = 0
+	local travel_speed = 20
+	local iter = 0
+	local ars_rccount = 0
+	function scan(node,path)		
+		while (node) and (direction) and (node ~= dest) do
+			local ars_speed
+			local ars_joint = Metrostroi.GetARSJoint(node,node.x+0.01,path or true)
+			if ars_joint then
+				local ARSLimit = ars_joint:GetMaxARS()
+				if ARSLimit >= 4  then
+					ars_speed = ARSLimit*10
+				end
+			end
+			
+			-- count and sum all ARS Limits 
+			if ars_speed then 
+				avg_speed = avg_speed + ars_speed 
+				ars_rccount = ars_rccount + 1
+			end
+			-- count and sum all node lengths
+			travel_dist = travel_dist + node.length
+			node = node.next
+			
+			if not node then break end
+			-- restrict calculations within another track
+			--if src.path == dest.path and node.branches and node.branches[1][2].path == src.path then scan(node,src.x > node.branches[1][2].x) end
+			--if src.path == dest.path and node.branches and node.branches[2] and node.branches[2][2].path == src.path then scan(node,src.x > node.branches[1][1].x) end
+			assert(iter < 10000, "OH SHI~")
+			iter = iter + 1
+		end
+		avg_speed = (avg_speed*0.20)/ars_rccount
+		travel_time = travel_dist/avg_speed
+	end
+	scan(src)
+	return travel_time,travel_dist
+end
+
 function ENT:Initialize()
 	self:SetModel("models/alexell/clock_arrive.mdl")
 	self:SetSolid(SOLID_VPHYSICS)
@@ -40,49 +85,69 @@ function ENT:Think()
 	end
 	
 	local MetrostroiTrainPositions = Metrostroi.TrainPositions or {}
-	local ArriveTimes = {}
-	local min_arr	
+	local NearestTrain
+	local NearestTrainNode
+	local min_dist
+	local ArriveTime	
 	
 	local StationPos = Metrostroi.GetPositionOnTrack(self.Platform.PlatformStart)
 	local StationPath = StationPos[1].node1.path.id	
+	local StationNodeID = StationPos[1].node1.id	
 	
-	for train, position in pairs(MetrostroiTrainPositions) do	
+	for train, position in pairs(MetrostroiTrainPositions) do
 		
-		local TrainStation = train:ReadCell(49160)
-		if TrainStation == 0 then TrainStation = train:ReadCell(49161) end
-		if TrainStation == 0 then continue end
 		local TrainPos = position[1]
-		local TrainPath = position[1].node1.path.id
+		if not TrainPos then continue end
+		
+		local TrainPath = TrainPos.node1.path.id
 		if not TrainPath then continue end	
 		
 		if StationPath != TrainPath then continue end
-		if math.abs(self.Station - TrainStation) > 2 then continue end
 		
+		local TrainNodeID = TrainPos.node1.id
+
 		local direction = TrainPos.x < StationPos[1].x
 		if not direction then continue end
 		
-		if StationPos[1] and TrainPos then			
-			local ArrTime,Dist = Metrostroi.GetTravelTime(TrainPos.node1, StationPos[1].node1)
+		--if math.abs(StationNodeID - TrainNodeID) > 450 then continue end
+		
+		if StationPos[1] and TrainPos then 
+			if not min_dist or math.abs(StationNodeID - TrainNodeID) < min_dist then
+				min_dist = StationNodeID - TrainNodeID
+				NearestTrain = train
+				NearestTrainNode = TrainPos.node1
+			end			
+		end
+
+		--[[if StationPos[1] and TrainPos then 
+			local ArrTime,Dist = GetTravelTime(TrainPos.node1, StationPos[1].node1)
 			if ArrTime < 600 then
 				table.insert(ArriveTimes, math.Round(ArrTime))
 			end
-		end			
+		end	]]
 	end
 	
-	if #ArriveTimes < 1 then 
-		min_arr = -1
+	--[[if #ArriveTimes < 1 then 
+		ArriveTime = -1
 	else
 		for k,v in pairs(ArriveTimes) do
-			if not min_arr or v < min_arr then 
-				min_arr = v 
+			if not ArriveTime or v < ArriveTime then 
+				ArriveTime = v 
 			end
 		end
+	end ]]
+	if NearestTrainNode and StationPos[1] then
+		ArriveTime = Metrostroi.GetTravelTime(NearestTrainNode, StationPos[1].node1)
+	else
+		ArriveTime = -1
 	end
-	self:SetNW2Int("ArriveTime",min_arr) 
+	if ArriveTime < 600 then
+		self:SetNW2Int("ArriveTime",ArriveTime) 
+	end
 	--[[ временно отключено, и вообще под вопросом 
 	net.Start("ClockArriveTime")
-	net.WriteInt(min_arr, 13)  
+	net.WriteInt(ArriveTime, 13)  
 	net.Broadcast()]]
-	self:NextThink(CurTime() + 1)
+	self:NextThink(CurTime() + 3)
 	return true
 end
